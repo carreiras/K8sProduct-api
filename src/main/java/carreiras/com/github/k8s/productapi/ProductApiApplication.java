f98f80e490a4ed0bebcd01eb6175efe4da797c25
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-08-22 21:00:21 -0300

    first commit

---
+++ src/main/java/carreiras/com/github/k8s/productapi/ProductApiApplication.java
@@ -0,0 +1,13 @@
+package carreiras.com.github.k8s.productapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/carreiras/com/github/k8s/productapi/ProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package carreiras.com.github.k8s.productapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
